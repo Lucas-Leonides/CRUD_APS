cd666c0e8b188c504a173ef53cc1442fad626855
Lucas-Leonides <142683570+Lucas-Leonides@users.noreply.github.com>
2024-12-19 13:50:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3000;
 const IP = process.env.IP
 
-app.listen(PORT, IP, () => { // Adicione '0.0.0.0' aqui
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-});
+})
